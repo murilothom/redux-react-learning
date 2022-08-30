5588be21e98690730fd1f7f580fa488c252d164b
Murilo Thom <murilothom.2002@hotmail.com>
2022-08-30 18:29:59 -0300

    edit final files

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,12 +1,9 @@
 import { combineReducers, configureStore } from '@reduxjs/toolkit'
-import contador from './contador'
-import modal from './modal'
 import login from './login'
 import localStorage from './middlewares/localStorage'
-// import logger from './middlewares/logger'
 
+const reducer = combineReducers({ login })
 
-const reducer = combineReducers({ contador, modal, login })
 const store = configureStore({
   reducer,
   middleware: getDefaultMiddleware => [...getDefaultMiddleware(), localStorage]
