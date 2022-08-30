595872e1d8e381a10054d845c01ff9568f107cc5
Murilo Thom <murilothom.2002@hotmail.com>
2022-08-30 17:21:02 -0300

    fetch token and user

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React, { useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import { login } from "./store/login";
 
 export function App() {
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
-  const { data } = useSelector(state => state.login.user)
   const dispatch = useDispatch()
 
   function handleSubmit(event) {
@@ -22,7 +21,6 @@ export function App() {
         <input id="password" type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
         <button style={{display: 'block', marginTop: '8px'}}>Enviar</button>
       </form>
-      <p>{data?.email}</p>
     </div>
   );
 }
--- src/store/login.js
+++ src/store/login.js
@@ -1,135 +1,48 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { combineReducers } from "@reduxjs/toolkit";
+import createAsyncSlice from "./helper/createAsyncSlice";
 
-const slice = createSlice({
-  name: 'login',
-  initialState: {
-    token: {
-      loading: false,
-      data: null,
-      error: null
-    },
-    user: {
-      loading: false,
-      data: null,
-      error: null
+const token = createAsyncSlice({
+  name: 'token',
+  fetchConfig(user) {
+    return { 
+      url: 'https://apidogs.murilothom.com/json/jwt-auth/v1/token',
+      options: {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(user)
+      }
     }
-  },
-  reducers: {
-    fetchTokenStarted(state) {
-      return {
-        ...state,
-        token: {
-          ...state.token,
-          loading: true
-        }
-      }
-    },
-    fetchTokenSuccess(state, action) {
-      return {
-        ...state,
-        token: {
-          ...state.token,
-          loading: false,
-          data: action.payload,
-          error: null
-        }
-      }
-    },
-    fetchTokenError(state, action) {
-      return {
-        ...state,
-        token: {
-          ...state.token,
-          loading: false,
-          data: null,
-          error: action.payload
-        }
-      }
-    },
-    fetchUserStarted(state) {
-      return {
-        ...state,
-        user: {
-          ...state.user,
-          loading: true
-        }
-      }
-    },
-    fetchUserSuccess(state, action) {
-      return {
-        ...state,
-        user: {
-          ...state.user,
-          loading: false,
-          data: action.payload,
-          error: null
-        }
-      }
-    },
-    fetchUserError(state, action) {
-      return {
-        ...state,
-        user: {
-          ...state.user,
-          loading: false,
-          data: null,
-          error: action.payload
-        }
-      }
-    },
-  }
+  } 
 })
 
-const {
-  fetchTokenStarted,
-  fetchTokenSuccess,
-  fetchTokenError,
-  fetchUserStarted,
-  fetchUserSuccess,
-  fetchUserError,
-
-  } = slice.actions
-
-export const fetchToken = (user) => async (dispatch) => {
-  try {
-    dispatch(fetchTokenStarted())
-    const response = await fetch('https://apidogs.murilothom.com/json/jwt-auth/v1/token', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(user)
-    })
-    const data = await response.json()
-    return dispatch(fetchTokenSuccess(data))
-  } catch (error) {
-    return dispatch(fetchTokenError(error.message))
-  }
-}
-
-export const fetchUser = (token) => async (dispatch) => {
-  try {
-    dispatch(fetchUserStarted())
-    const response = await fetch('https://apidogs.murilothom.com/json/api/user', {
-      method: 'GET',
-      headers: {
-        Authorization: 'Bearer ' + token
+const user = createAsyncSlice({
+  name: 'user',
+  fetchConfig(token) {
+    return { 
+      url: 'https://apidogs.murilothom.com/json/api/user',
+      options: {
+        method: 'GET',
+        headers: {
+          Authorization: 'Bearer ' + token
+        }
       }
-    })
-    const data = await response.json()
-    return dispatch(fetchUserSuccess(data))
-  } catch (error) {
-    return dispatch(fetchUserError(error.message))
-  }
-}
+    }
+  } 
+})
+
+const reducer = combineReducers({token: token.reducer, user: user.reducer})
+
+const fetchToken = token.asyncAction
+const fetchUser = user.asyncAction
+export default reducer
 
 export const login = (user) => async (dispatch) => {
   try {
     const { payload } = await dispatch(fetchToken(user))
-    if(payload.token !== undefined) await dispatch(fetchUser(payload.token))
+    payload.token !== undefined && await dispatch(fetchUser(payload.token))
   } catch (error) {
     
   }
 }
-
-export default slice.reducer
